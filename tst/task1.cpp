fe2a30492b612ccb2cb8dce507ec88e5568abd99
Roman Tabunnikov <rtab@mail.ru>
2020-05-23 18:48:32 +0300

    Task 1

--- tst/task1.cpp
+++ tst/task1.cpp
@@ -7,3 +7,16 @@ TEST(Task1Test, NPrimes) {
     calculate_primes(10, primes);
     EXPECT_THAT(primes, ::testing::ElementsAreArray({2, 3, 5, 7, 11, 13, 17, 19, 23, 29}));
 }
+
+TEST(Task1Test, NIsInvalid) {
+    EXPECT_THROW(
+        { 
+            int primes[10];
+            calculate_primes(0, primes);
+        }, 
+        std::invalid_argument);
+}
+
+TEST(Task1Test, PrimesIsNull) {
+    EXPECT_THROW({ calculate_primes(1, nullptr); }, std::invalid_argument);
+}
