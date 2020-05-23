67a3f0a22d54a10b96baf82ab1147c77dd94e377
Roman Tabunnikov <rtab@mail.ru>
2020-05-23 22:50:20 +0300

    Task 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "task2.h"
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    std::cout << "Hi there! Some tasks printout...\n\n";
+
+    std::cout << "Task 2: count number of words in text for each word length\n";
+    const char* text = "The quick brown fox jumps over the lazy dog!";
+    std::cout << text << std::endl;
+    auto result = count_words_by_length(text);
+    for (auto entry : result) {
+        std::cout << entry.second << " words with " << entry.first <<
+            (entry.first > 1 ? "-letters" : "letter") << " length" << std::endl;
+    }
 }
